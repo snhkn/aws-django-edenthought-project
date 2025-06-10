f3b6058302c2dea5f22b6916366ee029461c8e13
snhkn <senihaakin@gmail.com>
2025-06-10 19:42:58 +0300

    Set up urls, views, templates

--- edenthought/lynx/views.py
+++ edenthought/lynx/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+
+    return render(request, 'index.html')
+
+def register(request):
+
+    return render(request, 'register.html')
+
+def my_login(request):
+
+    return render(request, 'my-login.html')
+
+def dashboard(request):
+
+    return render(request, 'dashboard.html')
+
+def profile_management(request):
+
+    return render(request, 'profile-management.html')
